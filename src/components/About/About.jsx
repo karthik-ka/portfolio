4097996d92026d3a28cf7916a5d33a2dd8fc028e
karthik-ka <karthik.ka2001@gmail.com>
2023-07-11 15:24:54 +0530

    About section commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,14 @@
 import "./About.scss"
+import Info from "./Info"
 const About = () => {
   return (
-    <section className="about__section">
-
+    <section className="about__section section">
+      <h1 className="section__title">About Me</h1>
+      <span className="section__subtitle">My Introduction</span>
+      <div className="about__container grid">
+        <div className="about__img"></div>
+        <Info />
+      </div>
     </section>
   )
 }
